a63adce6b7d54baccc034638c12fb35085bba9c2
kozlov artem <artemko1987@yandex.ru>
2022-09-30 17:59:56 +0300

    задача1.выходные дни

--- main1.py
+++ main1.py
@@ -1,7 +1,7 @@
 day = int(input("Введи день недели: "))
 
 if day > 7 or day < 1:
-    print("Please, repeat the input")
+    print("Ошибка, введи заного")
 elif day == 6 or day == 7:
     print("Выходной")
 else:
