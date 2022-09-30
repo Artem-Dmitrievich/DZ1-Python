96e77a8614d64dce198d4fc7bf062119036d6351
kozlov artem <artemko1987@yandex.ru>
2022-09-30 17:53:28 +0300

    add задачу с выходными

--- main1.py
+++ main1.py
@@ -1,1 +1,8 @@
-print ('file 1')
+day = int(input("Введи день недели: "))
+
+if day > 7 or day < 1:
+    print("Please, repeat the input")
+elif day == 6 or day == 7:
+    print("Выходной")
+else:
+    print("Не выходной")
